35cc438c71325ac32c3dbac7f26691b1d00f495b
Allan Morales Prado <allan.moralesp@utem.cl>
2022-02-03 23:07:11 -0300

    Refinación de código: typos de pokemon y sus relaciones de daño

--- main.py
+++ main.py
@@ -52,35 +52,42 @@ print(pkmn_status)
 
 #fortalezas/debilidades
 #Ej: Charizard es tipo fuego, volador
-double_damage_from = []
-double_damage_to = []
-half_damage_from = []
-half_damage_to = []
-no_damage_from = []
-no_damage_to = []
+
+pkmn_damage_rel = {
+    'double_damage_from': [],
+    'double_damage_to': [],
+    'half_damage_from':[],
+    'half_damage_to': [],
+    'no_damage_from': [],
+    'no_damage_to': []
+}
 
 #tipo/tipo especial
 pkmn_type = [tipo['type']['url'] for tipo in resultado['types']]
-print(pkmn_type)
 
+#Funcion que captura tipos de pokemon y los almacena en una lista
+def add_types_rel(damage_types,type_src):
+    for ts in type_src:
+        damage_types.append(ts['url'])
+
+#Iteracion: por cada tipo el cual pertenece un pokemon
 for i in pkmn_type:
+    #Capturar el diccionario con que contenga la relacion de daño entre los tipos
     tipo = get_info(i)['damage_relations']
-    for j in tipo['double_damage_from']:
-        double_damage_from.append(j['url'])
-    for j in tipo['double_damage_to']:
-        double_damage_to.append(j['url'])
-    for j in tipo['half_damage_from']:
-        half_damage_from.append(j['url'])
-    for j in tipo['half_damage_to']:
-        half_damage_to.append(j['url'])
-    for j in tipo['no_damage_from']:
-        no_damage_from.append(j['url'])
-    for j in tipo['no_damage_to']:
-        no_damage_to.append(j['url'])
+    #Para cada lista del diccionario de relaciones de daño
+    for key in pkmn_damage_rel:
+        #Añadir los tipos de pokemon según la relación de daño correspondiente
+        add_types_rel(pkmn_damage_rel[key],tipo[key])
 
-traducir(double_damage_from)
-double_damage_from = list(set(double_damage_from))
-print(double_damage_from)
+#Para cada clave del diccionario de relaciones de daño
+for i in pkmn_damage_rel:
+    #Eliminar redundancias de las listas asociadas a cada clave
+    pkmn_damage_rel[i] = list(set(pkmn_damage_rel[i]))
+    #Traducir la URL del tipo al nombre del tipo en lenguaje "es"
+    traducir(pkmn_damage_rel[i])
+    #Comprobar los efectos de la iteración (opcional)
+    print(pkmn_damage_rel[i])
+
 '''
 print(double_damage_from)
 print(half_damage_to)
