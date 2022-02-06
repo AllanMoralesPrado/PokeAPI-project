88c4c48a1e2ade3d568c5b3b202e9a43736e3c75
Allan Morales Prado <allan.moralesp@utem.cl>
2022-02-06 17:32:12 -0300

    datos de prueba en modulo show

--- show.py
+++ show.py
@@ -2,12 +2,6 @@
 import webbrowser
 import os
 import time
-from get_evo import get_evolution
-from build_evo_html import build_evo_html
-from build_pokemon_html import build_html
-from get_base_info import get_base_pokemon
-from get_species_info import get_species
-from get_types import get_types_info
 
 def show_pics(html, nombre):
     with open(f'{nombre}.html','w',encoding='utf8') as f:
@@ -17,4 +11,22 @@ def show_pics(html, nombre):
     webbrowser.open(f'{nombre}.html')
     time.sleep(5)
     os.remove(f'{nombre}.html')
+
+if __name__ == '__main__':
+    from get_evo import get_evolution
+    from build_evo_html import build_evo_html
+    from build_pokemon_html import build_html
+    from get_base_info import get_base_pokemon
+    from get_species_info import get_species
+    from get_types import get_types_info
+
+    name = 'gardevoir'
+    id, weight, stats, sprite = get_base_pokemon(name)
+    pre_evolution, description = get_species(name)
+    pokemon_tipo, pokemon_tipo_en, tipo_especial, pkmn_buffs_n_nerfs = get_types_info(name)
+
+    html = build_html(id,name,weight,stats,sprite,pre_evolution,description,pokemon_tipo,pokemon_tipo_en,tipo_especial,pkmn_buffs_n_nerfs)
+    show_pics(html,'output_alpha')
     
+    html = build_evo_html(name, get_evolution(name))
+    show_pics(html,'evo_output_alpha')
